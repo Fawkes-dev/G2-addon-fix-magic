ce1fb72c943d5088cb94841f8711bdc5942c0ed8
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2023-11-20 14:07:26 +0100

    fix spell-casting-exploit for SPL_INFLATE and SPL_SLEEP

--- _work/Data/Scripts/Content/AI/Magic/Spells/Spell_Sleep.d
+++ _work/Data/Scripts/Content/AI/Magic/Spells/Spell_Sleep.d
@@ -26,25 +26,6 @@ func int Spell_Logic_Sleep(var int manaInvested) // Parameter manaInvested wird 
 			self.attribute[ATR_MANA] = self.attribute[ATR_MANA] - SPL_Cost_Sleep;
 		};
 
-		if ((!C_BodyStateContains(other, BS_SWIM))
-		&& (!C_BodyStateContains(other, BS_DIVE))
-		&& (!C_NpcIsDown(other))
-		&& (other.guild < GIL_SEPERATOR_HUM)
-		// && (other.level - self.level <= 3) // EGAL, sonst Spell witzlos (Klau-Wachen)
-		&& (other.flags != NPC_FLAG_IMMORTAL)
-		&& (Npc_GetDistToNpc(self, other) <= 1000)
-		// && (C_NpcIsGateGuard (self) == FALSE) // EGAL, weil PAL oder Kdf kritische Wachen sind
-		&& (other.guild != GIL_KDF)
-		&& (other.guild != GIL_DMT)
-		&& (other.guild != GIL_PAL))
-		{
-			Npc_SetTarget(other, self);
-			other.aivar[AIV_StateTime] = 0;
-			Npc_ClearAIQueue(other);
-			B_ClearPerceptions(other);
-			AI_StartState(other, ZS_MagicSleep, 0, "");
-		};
-
 		return SPL_SENDCAST; // Spell wird auch gecasted, wenn keine Auswirkungen (other geht nicht in ZS) Mana is dann weg - Pech gehabt! (soll so sein!)
 	}
 	else // nicht genug Mana
@@ -56,6 +37,25 @@ func int Spell_Logic_Sleep(var int manaInvested) // Parameter manaInvested wird 
 func void Spell_Cast_Sleep()
 {
 	// self.attribute[ATR_MANA] = self.attribute[ATR_MANA] - SPL_Cost_Sleep; // nicht drin, wegen Kommentar oben
+
+	if ((!C_BodyStateContains(other, BS_SWIM))
+	&& (!C_BodyStateContains(other, BS_DIVE))
+	&& (!C_NpcIsDown(other))
+	&& (other.guild < GIL_SEPERATOR_HUM)
+	// && (other.level - self.level <= 3) // EGAL, sonst Spell witzlos (Klau-Wachen)
+	&& (other.flags != NPC_FLAG_IMMORTAL)
+	&& (Npc_GetDistToNpc(self, other) <= 1000)
+	// && (C_NpcIsGateGuard (self) == FALSE) // EGAL, weil PAL oder Kdf kritische Wachen sind
+	&& (other.guild != GIL_KDF)
+	&& (other.guild != GIL_DMT)
+	&& (other.guild != GIL_PAL))
+	{
+		Npc_SetTarget(other, self);
+		other.aivar[AIV_StateTime] = 0;
+		Npc_ClearAIQueue(other);
+		B_ClearPerceptions(other);
+		AI_StartState(other, ZS_MagicSleep, 0, "");
+	};
 
 	self.aivar[AIV_SelectSpell] += 1;
 };
